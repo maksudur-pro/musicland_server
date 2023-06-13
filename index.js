55dede3e76b1c94fa657b441d87ad0f41b7a79b6
unknown <ajoy9532@gmail.com>
2023-06-13 18:11:20 +0600

    added vercel json

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require("dotenv").config();
 const port = process.env.PORT || 5000;
 const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
 // middleware
